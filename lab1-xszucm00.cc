bc13ccc9b835cefd748164010c61444a6f8eef70
Martin Szuc <mszuc@redhat.com>
2025-03-12 14:00:51 +0100

    cv5: added Hello World

---
+++ lab1-xszucm00.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "ns3/core-module.h"
+
+using namespace ns3;
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Authored by Martin Szuc 231284" << std::endl;
+    return 0;
+}
